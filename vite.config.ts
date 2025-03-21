eb2302cb8901737b51394126bf478d7e60b99181
PHTMGatt <phtmgatt@gmail.com>
2025-03-20 20:15:57 -0400

    fixes for render deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   envDir: './environment',
+//   plugins: [react()],
+// });
+
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   envDir: './environment',
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Bind to 0.0.0.0 so Render can access the server           // Use Render's PORT or fallback to 3000 locally
+  },
 });
+
